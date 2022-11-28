7e5175694bb19aa25d0ce5553873f2325d060a58
Eyal Cohen <eyalcohen4.ec@gmail.com>
2022-11-28 14:49:19 +0200

    Remove css-selector-generator

--- src/dom/selectors.ts
+++ src/dom/selectors.ts
@@ -1,20 +1,7 @@
 import { finder } from './finder';
 
 export function getSelector(event: Event, dataAttribute?: string): string {
-  const cssSelector = require('css-selector-generator');
   if (event.target instanceof Element) {
-    if (dataAttribute && event.target.getAttribute(dataAttribute)) {
-      return `[${dataAttribute}="${event.target.getAttribute(dataAttribute)}"]`;
-    }
-
-    const candidate = cssSelector?.getCssSelector(event.target) as
-      | string
-      | null;
-
-    if (candidate) {
-      return candidate;
-    }
-
     return finder(event.target);
   }
 
