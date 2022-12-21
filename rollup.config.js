66b38263281b1ca6050795a2d14ce2ebffe7f751
Eyal Cohen <eyalcohen4.ec@gmail.com>
2022-12-21 13:08:55 +0200

    Add decleration files

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import dts from 'rollup-plugin-dts';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
@@ -6,31 +7,38 @@ import { terser } from 'rollup-plugin-terser';
 
 const pkg = require('./package.json');
 
-export default {
-  input: `src/index.ts`,
-  output: [
-    {
-      file: `lib/index.js`,
-      format: 'cjs',
-      sourcemap: false,
-      plugins: [],
-    },
-  ],
-  external: [],
-  watch: {
-    include: 'src/**',
-  },
-  plugins: [
-    dotenv(),
-    typescript({ useTsconfigDeclarationDir: true }),
-    commonjs({ extensions: ['.js', '.ts'] }),
-    terser({
-      compress: true,
-      mangle: {
-        keep_classnames: false,
-        keep_fnames: false,
+export default [
+  {
+    input: `src/index.ts`,
+    output: [
+      {
+        file: `lib/index.js`,
+        format: 'cjs',
+        sourcemap: false,
+        plugins: [],
       },
-    }),
-    resolve(),
-  ],
-};
+    ],
+    external: [],
+    watch: {
+      include: 'src/**',
+    },
+    plugins: [
+      dotenv(),
+      typescript({ useTsconfigDeclarationDir: true }),
+      commonjs({ extensions: ['.js', '.ts'] }),
+      terser({
+        compress: true,
+        mangle: {
+          keep_classnames: false,
+          keep_fnames: false,
+        },
+      }),
+      resolve(),
+    ],
+  },
+  {
+    input: 'src/index.ts',
+    output: [{ file: 'lib/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+];
